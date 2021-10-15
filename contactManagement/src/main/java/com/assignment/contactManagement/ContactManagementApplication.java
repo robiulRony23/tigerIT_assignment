04be143ea411217fb92c3bacc9ad516b398de8d8
Rony <rony23.cse.du@gmail.com>
2021-10-15 16:54:35 +0600

    adding for  war file

--- contactManagement/src/main/java/com/assignment/contactManagement/ContactManagementApplication.java
+++ contactManagement/src/main/java/com/assignment/contactManagement/ContactManagementApplication.java
@@ -2,9 +2,16 @@ package com.assignment.contactManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ContactManagementApplication {
+public class ContactManagementApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ContactManagementApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactManagementApplication.class, args);
